7601021a45645667f18504845981ef93999bb6fd
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-03-07 20:42:39 -0500

    correxiones_fincas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import {Fincas} from "./pages/Fincas.jsx"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { RegistrarFincas } from "./pages/RegistrarFincas.jsx"
-import { ActualizarFincas } from "./pages/ActualizarFincas.jsx"
 import { RegistrarLotes } from "./pages/RegistrarLotes.jsx"
 import { ActualizarLotes } from "./pages/ActualizarLotes.jsx"
 import { Lotes } from "./pages/Lotes.jsx"
+import { ActualizarFincas } from "./pages/ActualizarFincass.jsx"
+import { RegistrarFincas } from "./pages/RegistrarFincass.jsx"
+import { Fincas } from "./pages/Fincass.jsx"
 
 function App() {
 
