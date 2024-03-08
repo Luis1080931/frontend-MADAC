a2dd162818c2d4f67f7a8b671ad8349624d20ef7
MariaTorres7 <maria1033097029@gmail.com>
2024-03-07 23:59:28 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,26 @@ import { AnalisisRegistrar } from './pages/AnalisisRegistrar.jsx';
 import { AnalisisActualizar } from './pages/AnalisisActualizar.jsx';
 
 
+import { VistaVariedades } from './pages/VistaVariedades.jsx';
+import { VariedadesRegistrar } from './pages/VariedadesRegistrar.jsx';
+import { VariedadesActualizar } from './pages/VariedadesActualizar.jsx';
+
+
+
+
 function App() {
 
   return (
     <BrowserRouter>    
     <Routes>
-    <Route path='/' element={<VistaAnalisis/>} />
+    {/* <Route path='/' element={<VistaAnalisis/>} />
       <Route path='/analisisregistrar' element={<AnalisisRegistrar/>} />
-      <Route path='/analisisactualizar' element={<AnalisisActualizar/>} />
+      <Route path='/analisisactualizar' element={<AnalisisActualizar/>} /> */}
+
+      
+    <Route path='/' element={<VistaVariedades/>} />
+      <Route path='/variedadesregistrar' element={<VariedadesRegistrar/>} />
+      <Route path='/variedadesactualizar' element={<VariedadesActualizar/>} />
      </Routes>
       {/* <VistaAnalisis/> */}
 
